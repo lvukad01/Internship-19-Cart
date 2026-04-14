763b391c29a9cec829ac2f669ab547870959cecf
lvukad01 <lana.vukadin1234@gmail.com>
2026-04-15 01:26:10 +0200

    Change baseURL

--- apps/fe/src/api/axiosInstance.tsx
+++ apps/fe/src/api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', 
+  baseURL: 'http://51.20.85.113:3000/api', 
 });
 
 api.interceptors.request.use((config)=>{
